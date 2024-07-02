a9fbc04bfb746712fe039b1e69c811ecf8a11d01
Ruben Hernandez <rubdog52014@gmail.com>
2024-07-01 19:22:09 -0700

    User enters a number in cents and output prints how many of each coin

---
+++ change.py
@@ -0,0 +1,13 @@
+print("Please enter an amount in cents less than a dollar.")
+num=int(input())
+quarter=num//25
+dime=num%25//10
+nickel=num%25%10//5
+penny=num%25%10%5//1
+print("Your change will be:")
+print("Q:", quarter)
+print("D:", dime)
+print("N:", nickel)
+print("P:", penny)
+
+
